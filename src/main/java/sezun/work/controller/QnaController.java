eac636885fcf5a4b9312ab266fb72be4feb66cc6
HJtheKing <09hyen28@gmail.com>
2023-02-08 00:11:22 +0900

    0208

--- src/main/java/sezun/work/controller/QnaController.java
+++ src/main/java/sezun/work/controller/QnaController.java
@@ -1,8 +1,16 @@
 package sezun.work.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sezun.work.domain.Qna;
 import sezun.work.service.QnaService;
 
+import java.util.List;
+
+@Controller
 public class QnaController {
 
     private final QnaService qnaService;
@@ -10,5 +18,27 @@ public class QnaController {
     @Autowired
     public QnaController(QnaService qnaService){
         this.qnaService = qnaService;
+    }
+
+    @GetMapping(value = "/qna")
+    public String createForm(){
+        return "Qna/QnaForm";
+    }
+    @PostMapping(value = "/qna")
+    public String register(QnaForm form){
+        Qna qna = new Qna();
+        qna.setCorpName(form.getCorpName());
+        qna.setName(form.getName());
+        qna.setEmail(form.getEmail());
+
+        qnaService.register(qna);
+        Qna result = qnaService.findOne(1L).get();
+        return "redirect:/";
+    }
+    @GetMapping(value = "/qna/list")
+    public String list(Model model){
+        List<Qna> list = qnaService.findQna();
+        model.addAttribute("Qnas",list);
+        return "Qna/QnaList";
     }
 }
---
+++ src/main/java/sezun/work/controller/QnaForm.java
@@ -0,0 +1,31 @@
+package sezun.work.controller;
+
+public class QnaForm {
+    private String corpName;
+    private String name;
+    private String email;
+
+    public String getCorpName() {
+        return corpName;
+    }
+
+    public void setCorpName(String corpName) {
+        this.corpName = corpName;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
